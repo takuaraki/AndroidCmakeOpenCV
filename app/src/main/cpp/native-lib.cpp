8123b37608c15d88f845f8265a7735906ee03b68
takuaraki <takuaraki7@gmail.com>
2017-05-22 01:26:13 +0900

    display columns and rows of new Mat

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,15 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core.hpp>
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_example_com_androidcmakeopencv_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    cv::Mat mat;
+    std::ostringstream oss;
+    oss << "rows: " << mat.rows << ", cols: " << mat.cols;
+    std::string str = oss.str();
+    return env->NewStringUTF(str.c_str());
 }
